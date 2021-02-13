d7ea86d2a1c653fcfdc88167ca53460b124fc693
Rafael Araujo <rafael.araujo@vestiairecollective.com>
2021-02-13 23:53:55 +0100

    Deleting MainActivity.kt and using HomeActivity

--- features/profile-impl/build/generated/source/buildConfig/debug/com/orafaraujo/features/profileimpl/BuildConfig.java
+++ features/profile-impl/build/generated/source/buildConfig/debug/com/orafaraujo/features/profileimpl/BuildConfig.java
@@ -5,6 +5,6 @@ package com.orafaraujo.features.profileimpl;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String LIBRARY_PACKAGE_NAME = "com.orafaraujo.features.profile";
+  public static final String LIBRARY_PACKAGE_NAME = "com.orafaraujo.features.profileimpl";
   public static final String BUILD_TYPE = "debug";
 }
