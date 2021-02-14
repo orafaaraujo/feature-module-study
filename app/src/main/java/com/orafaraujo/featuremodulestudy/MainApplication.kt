5f824c4a15f00fae8020175dcb74719706aad918
Rafael Araujo <rafael.araujo@vestiairecollective.com>
2021-02-14 12:44:12 +0100

    Adding MainApplication.kt

---
+++ app/src/main/java/com/orafaraujo/featuremodulestudy/MainApplication.kt
@@ -0,0 +1,11 @@
+package com.orafaraujo.featuremodulestudy
+
+import android.app.Application
+
+class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
